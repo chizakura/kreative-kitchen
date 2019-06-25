f0f188ea7e0bbfe7fbffb8107a396a0b1e59dc18
Linda Chin <lindapchin225@gmail.com>
2019-06-25 15:36:19 -0400

    finished setting up data call

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,8 @@ import SearchBar from './SearchBar';
 import Recipe from './Recipe';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       searchItems: "",
@@ -29,25 +29,41 @@ class App extends Component {
   async handleSubmit(event) {
     event.preventDefault();
     const res = await axios(`https://api.edamam.com/search?q=${this.state.searchItems}&app_id=6c3aa117&app_key=0a7b9cbed799150826ba8a7d204a2382&time=1-60`);
-    console.log(res.data.hits)
+    const list = res.data.hits.map(recipe => {
+      return recipe.recipe
+    })
+    this.setState({
+      recipeList: list,
+      searchItems: ""
+    })
   }
 
   render() {
     console.log(this.state.searchItems)
-    console.log(this.state.searchItemsArr)
+    console.log(this.state.recipeList)
     return (
       <div className="App">
         <div className="nav">This is div</div>
         <SearchBar
           handleChange = {this.handleChange}
           handleSubmit = {this.handleSubmit}
+          searchItems = {this.state.searchItems}
         />
-        
         {this.state.recipeList.length === 0 ? 
           (<div>
             <h1>Enter an ingredient.</h1>
             <p>Entering more will narrow down your results.</p>
-          </div>) : <Recipe/>
+          </div>) : 
+          (<div>
+            {this.state.recipeList.map((recipe, index) => {
+              return (
+                <Recipe
+                  key = {index}
+                  recipe = {recipe}
+                />
+              )
+            })}
+          </div>)
         }
       </div>
     )
--- client/src/Recipe.js
+++ client/src/Recipe.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Recipe = () => {
-    return (
-        <div>
-            <h1>Recipe Block</h1>
-        </div>
-    )
+class Recipe extends Component {
+    render() {
+        return (
+            <div className="recipe-box">
+                <img height="95px" src={this.props.recipe.image} alt={this.props.recipe.label}/>
+                <div className="recipe-details">
+                    <p>{this.props.recipe.label}</p>
+                    <p>{`${this.props.recipe.totalTime} minutes`}</p>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Recipe;
