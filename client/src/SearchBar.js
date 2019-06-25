e9b73d217a4f0c7fb7df5d22f0e36de9db58874b
Linda Chin <lindapchin225@gmail.com>
2019-06-25 13:27:13 -0400

    added components and some css

---
+++ client/src/SearchBar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <div>
+                <input type="text" placeholder="Example: green pepper"/>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
