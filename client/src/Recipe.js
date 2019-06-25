740bb377f55264105267a2a3f1287dfe320053fd
Linda Chin <lindapchin225@gmail.com>
2019-06-25 19:21:29 -0400

    small changes

--- client/src/Recipe.js
+++ client/src/Recipe.js
@@ -7,7 +7,7 @@ class Recipe extends Component {
                 <img height="95px" src={this.props.recipe.image} alt={this.props.recipe.label}/>
                 <div className="recipe-details">
                     <p className="recipe-title">{this.props.recipe.label}</p>
-                    <p className="recipe-time">{`${this.props.recipe.totalTime} minutes`}</p>
+                    <p className="recipe-time">{`${this.props.recipe.totalTime} MINUTES`}</p>
                     <p className="recipe-summary">Summary of recipe</p>
                 </div>
             </div>
