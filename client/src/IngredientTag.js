147fe258d595c6ed7298ff6b2e2df40b3137ce1f
KYAN <briansouza209@gmail.com>
2019-06-25 17:27:08 -0400

    added ingredient tag component stucture

---
+++ client/src/IngredientTag.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './App.css';
+
+
+class IngredientTag extends Component {
+    render() {
+        return (
+            <div className="ingredient-tag-container">
+              Test
+            </div>
+        )
+    }
+}
+
+export default IngredientTag;
