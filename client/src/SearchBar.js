bb7578cff7cae218c529c20138f2cd4db7bca665
Demry Mebane <demrymebane@gmail.com>
2019-06-25 16:20:54 -0400

    Adding .css

--- client/src/SearchBar.js
+++ client/src/SearchBar.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
 import './App.css';
+import Search from "./Search.png";
 
 class SearchBar extends Component {
     render() {
         return (
-            <div>
-                <input type="text" className="text-box" placeholder="Example: green pepper"/>
+            <div className = "search-bar-container">
+                <div className="Search">
+                    <img src={Search} alt = "Search"/>
+                <input type="text" className="text-box" placeholder="Example: Green Pepper"/>
+
+                <div className = "ingredient-text">
+                <div className = "enter-an-ingredient"> Enter an ingredient. </div>
+                    <div className = "entering-more-will"> Entering more will narrow down your results.</div>
+                </div>
             </div>
+            </div>
+
         )
     }
 }
