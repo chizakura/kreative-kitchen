43722afa0d4e25f47e766c9fc0c33ac7dbba8bf0
Linda Chin <lindapchin225@gmail.com>
2019-06-26 22:58:58 -0400

    working on styling search results for desktop

--- client/src/Recipe.js
+++ client/src/Recipe.js
@@ -4,7 +4,7 @@ class Recipe extends Component {
     render() {
         return (
             <div className="recipe-box">
-                <img height="95px" src={this.props.recipe.image} alt={this.props.recipe.label}/>
+                <img src={this.props.recipe.image} alt={this.props.recipe.label}/>
                 <div className="recipe-details">
                     <p className="recipe-title">{this.props.recipe.label}</p>
                     <p className="recipe-time">{`${this.props.recipe.totalTime} MINUTES`}</p>
